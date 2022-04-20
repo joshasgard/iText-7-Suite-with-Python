6980b1dbb2002c199ca01b48d8502503886d9753
joshasgard <odesanmiidowu@gmail.com>
2022-04-20 04:43:34 +0100

    Initial Commit with pdf-to-image

---
+++ pdf_to_image.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+API_URL = "http://18.224.190.115:8080/api/job/"
+
+payload = {
+    "job": {
+        "request": {
+            "type": "PDF_TO_IMAGES",
+            "imageType": "JPEG",
+            "pageNumbers": "1",
+            "defaultPageScaling": 1,
+        },
+        "sourceFiles": {
+            "checksum": "a0078eae317194bf913db457b16a54fd",
+            "fileType": "application/pdf",
+            "fileSize": 318046,
+            "fileName": "NPHCDA- COVID-19.pdf",
+        },
+    }
+}
+
+headers = {
+    "Content-Type": "multipart/form-data;boundary=----arbitraryboundary",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+}
+
+files = {"file": open("NPHCDA- COVID-19.pdf", "rb")}
+
+
+response = requests.post(API_URL, data=payload, files=files, headers=headers)
+
+print(response.text)
