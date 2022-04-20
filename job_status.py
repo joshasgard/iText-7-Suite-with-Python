5f76c2963d6991138b460bd1c213583833bd01fe
joshasgard <odesanmiidowu@gmail.com>
2022-04-20 06:02:38 +0100

    Complete polling mechanism

--- job_status.py
+++ job_status.py
@@ -1,35 +1,36 @@
-from ast import arg
 import requests
 import polling
-import logging
 
 API_URL = "http://18.224.190.115:8080/api/job/"
-job_id = EXAMPLE_JOB_ID
+job_id = "421223fb-e363-43fc-b4fe-4e2aa36f84ed"  # Check your job id from your post request response
 
-status_url = "".join([API_URL, job_id])
-get_file_url = "".join([status_url, "/file/result"])
+status_url = API_URL + job_id
+# url_try = "http://18.224.190.115:8080/api/job/421223fb-e363-43fc-b4fe-4e2aa36f84ed"
+get_file_url = status_url + "file/result"
 
 # Polling status and download
-def custom_request(url: str):
-    if requests.get(url=url).json().get("status") != "FINISHED":
-        logging.info("Polling started.......")
-        logging.info("Waiting.......")
-        return False
-    return True
+def test(response):
+    return response.status_code == 200
 
 
 def main():
     try:
         # Check result status
-        polling.poll(custom_request, step=60, args=(status_url), poll_forever=True)
-        print("Got status as Finished. Proceeding....")
+        polling.poll(
+            lambda: requests.get(status_url),
+            step=60,
+            poll_forever=True,
+            check_success=test,
+        )
+        print("Job is Finished. Proceeding to download....")
+
         # Download result when status is Finished
         download_result = requests.get(get_file_url)
         with open("result.jpg", "wb") as f:
             f.write(download_result.content)
-        exit()
-    except:
-        logging.info("exiting")
+
+    except KeyboardInterrupt:
+        print("exiting")
         exit()
 
 
